e0529eeddf290e355441dc134f8790669f8122a0
Muammer Akca <muammerakca10@gmail.com>
2022-10-08 10:32:43 +0300

    Project Created

---
+++ src/main/java/com/mycompany/ebebek_java_qa_practicum_task/Ebebek_Java_QA_Practicum_Task.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ebebek_java_qa_practicum_task;
+
+/**
+ *
+ * @author air
+ */
+public class Ebebek_Java_QA_Practicum_Task {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
