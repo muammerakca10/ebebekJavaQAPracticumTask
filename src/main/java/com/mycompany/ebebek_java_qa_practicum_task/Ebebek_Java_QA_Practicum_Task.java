2998348e7cb39b1f276b20e938a23632381975a0
Muammer Akca <muammerakca10@gmail.com>
2022-10-25 23:41:33 +0300

    Task finished.

--- src/main/java/com/mycompany/ebebek_java_qa_practicum_task/Ebebek_Java_QA_Practicum_Task.java
+++ src/main/java/com/mycompany/ebebek_java_qa_practicum_task/Ebebek_Java_QA_Practicum_Task.java
@@ -1,17 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
 
 package com.mycompany.ebebek_java_qa_practicum_task;
 
-/**
- *
- * @author air
- */
 public class Ebebek_Java_QA_Practicum_Task {
 
     public static void main(String[] args) {
+        
+        Employee employee = new Employee("kemal", 2000, 45, 1985);
+        System.out.println( employee.toString());
         
     }
 }
---
+++ src/main/java/com/mycompany/ebebek_java_qa_practicum_task/Employee.java
@@ -0,0 +1,96 @@
+package com.mycompany.ebebek_java_qa_practicum_task;
+
+//import java.time.LocalDateTime;
+        
+public class Employee{
+
+    private String name;
+    private double salary;
+    private int workHours;
+    private int hireYear;
+    static final int MAX_HOURS = 40;
+    //static private LocalDateTime nowYear = LocalDateTime.now();
+    static final int NOW_YEAR = 2021;
+
+    public Employee(String name, double salary, int workHours, int hireYear) {
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+        //System.out.println("Calistigi yil : " +(nowYear.getYear() - hireYear) );
+    }
+    
+    public double tax(double salary){
+        if (this.salary < 1000){
+            return 0;
+        } else {
+            return this.salary *0.03;
+        }
+    }
+    
+    public double bonus(int workHours){
+        if(workHours > 40) {
+            return (workHours-40)*30;
+        } else {
+            return 0;
+        }
+    }
+    
+    public double raiseSalary(int hireYear, double salary){
+        int totalWorkYear = NOW_YEAR - hireYear;
+        if (totalWorkYear > 0 && totalWorkYear <= 9){
+            return salary * 0.05;
+        } else if (totalWorkYear > 9 && totalWorkYear <= 19){
+            return salary * 0.1;
+        } else {
+            return salary * 0.15;
+        }
+    }
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getWorkHours() {
+        return workHours;
+    }
+
+    public void setWorkHours(int workHours) {
+        this.workHours = workHours;
+    }
+
+    public int getHireYear() {
+        return hireYear;
+    }
+
+    public void setHireYear(int hireYear) {
+        this.hireYear = hireYear;
+    }
+    @Override
+    public String toString() {
+        return "Adı : " + this.name + 
+                "\nMaaşı : " + this.salary + 
+                "\nÇalışma Saati : " + this.workHours +
+                "\nBaşlangıç Yılı : " + this.getHireYear() +
+                "\nVergi : " + this.tax(this.salary) +
+                "\nBonus : " + this.bonus(this.workHours) +
+                "\nMaaş Artışı : " + this.raiseSalary(this.hireYear, this.salary) +
+                "\nVergi ve bonuslarla toplam maaş : " + (this.salary - tax(this.salary) + bonus(this.workHours))+
+                "\nToplam Maaş : " +(this.salary - tax(this.salary) + bonus(this.workHours) + this.raiseSalary(this.hireYear, this.salary));
+    }
+    
+}
