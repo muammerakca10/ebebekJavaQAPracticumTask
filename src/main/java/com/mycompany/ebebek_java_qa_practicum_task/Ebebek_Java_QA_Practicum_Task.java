af6d9f7991db403bbfbc35983e91e978d65bc9a9
Muammer Akca <muammerakca10@gmail.com>
2022-10-25 19:42:56 +0300

    Added Interface Employee

--- src/main/java/com/mycompany/ebebek_java_qa_practicum_task/Ebebek_Java_QA_Practicum_Task.java
+++ src/main/java/com/mycompany/ebebek_java_qa_practicum_task/Ebebek_Java_QA_Practicum_Task.java
@@ -12,6 +12,6 @@ package com.mycompany.ebebek_java_qa_practicum_task;
 public class Ebebek_Java_QA_Practicum_Task {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
